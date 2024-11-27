37c15071a6ea6c65b1be8ee56ead3852d6b46b53
LEE JEONG TAEK <leejeongtak@gmail.com>
2024-11-27 11:28:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # from dotenv import load_dotenv
 # load_dotenv()
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+# from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 # from langchain.llms import OpenAI
 # llm = OpenAI()
 # result = llm.predict("hi")
 # print(result)
 
-chat_model = ChatOpenAI()
+chat_model = ChatOpenAI(model_name="gpt-3.5-turbo")
+# result = chat_model.predict("코딩에 대한 시를 써줘")
+# print(result)
 
 st.title('인공지능 시인')
 
@@ -18,4 +21,3 @@ if st.button('시 작성하기'):
     with st.spinner('시 작성중...'):
         result = chat_model.predict(content+"에 대한 시를 써줘")
         st.write(result)
-
